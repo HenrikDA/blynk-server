03bc5e48cdd8335b05fefcfaeb304904661c6b5b
Dmitriy Dumanskiy <dmitriy@blynk.cc>
2015-03-05 01:49:59 +0200

    More scripts

---
+++ scripts/copy_to_azure_server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp ../build/server-0.1.jar azureuser@blynk-test-east.cloudapp.net:/home/azureuser
---
+++ scripts/copy_to_local_server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp ../build/server-0.1.jar pupkin@192.168.0.64:/home/pupkin/Dropbox/blynk_new
