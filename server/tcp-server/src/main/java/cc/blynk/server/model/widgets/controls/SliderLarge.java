af68503dc4456688d228ccc6facc5b2f7916eb2e
Dmitriy Dumanskiy <dmitriy@blynk.cc>
2015-03-24 17:24:44 +0200

    Slider large fix.

--- server/tcp-server/src/main/java/cc/blynk/server/model/widgets/controls/SliderLarge.java
+++ server/tcp-server/src/main/java/cc/blynk/server/model/widgets/controls/SliderLarge.java
@@ -1,18 +1,10 @@
 package cc.blynk.server.model.widgets.controls;
 
-import cc.blynk.server.model.widgets.Widget;
-
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 21.03.15.
  */
-public class SliderLarge extends Widget {
-
-    Boolean pwm;
-
-    Integer min;
-
-    Integer max;
+public class SliderLarge extends Slider {
 
 }
